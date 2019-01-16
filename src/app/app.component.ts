c5c807fa9205c8ed8118f57f82abebace533f178
SR <srwhitebox@gmail.com>
2019-01-17 00:55:24 +0800

    上傳初始版本
    進度為章節2-14

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>{{title}}</h1>
+  <products></products>
+  `
+})
+export class AppComponent {
+  title: string = 'My First Angular App';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './Products.Component';
+import { ProductService } from './product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent, ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
